7b1eb504259513c1586e2aae3feb5510f4b1d952
vaclav2016 <vaclav@openmailbox.org>
2016-08-30 15:46:56 +0000

    .

--- xmppredis.c
+++ xmppredis.c
@@ -170,7 +170,7 @@ int message_handler(xmpp_conn_t * const conn, xmpp_stanza_t * const stanza, void
 		int bufLen = strlen(insert_cmd) + strlen(replytext) + strlen(redisQueueInbound);
 		char *buf = malloc(bufLen);
 		snprintf(buf, bufLen, insert_cmd, redisQueueInbound, replytext);
-printf("%s\n", buf);
+// printf("%s\n", buf);
 		rreply = redisCommand(rc, buf);
 		freeReplyObject(rreply);
 
@@ -336,33 +336,18 @@ int main(int argc, char **argv) {
 	snprintf(get_cmd, sizeof(get_cmd), get_cmd_tmpl, redisQueueOutbound);
 	snprintf(subscribe_cmd, sizeof(subscribe_cmd), subscribe_cmd_tmpl, redisQueueOutbound);
 
-	/* init library */
 	xmpp_initialize();
 
-	/* create a context */
 	log = xmpp_get_default_logger(XMPP_LEVEL_DEBUG); /* pass NULL instead to silence output */
 	ctx = xmpp_ctx_new(NULL, log);
 
-	/* create a connection */
 	conn = xmpp_conn_new(ctx);
 //	xmpp_conn_set_keepalive(conn, KA_TIMEOUT, KA_INTERVAL);
 
-	/*
-	 * also you can disable TLS support or force legacy SSL
-	 * connection without STARTTLS
-	 *
-	 * see xmpp_conn_set_flags() or examples/basic.c
-	 */
-
-	/* setup authentication information */
 	xmpp_conn_set_jid(conn, jid);
 	xmpp_conn_set_pass(conn, pass);
 
-	/* initiate connection */
 	xmpp_connect_client(conn, NULL, 0, conn_handler, ctx);
-
-	/* enter the event loop - 
-	our connect handler will trigger an exit */
 
 	args.ctx = ctx;
 	args.conn = xmpp_conn_clone(conn);
@@ -370,11 +355,9 @@ int main(int argc, char **argv) {
 	pthread_create(&thread, NULL, redisClient, &args);
 	xmpp_run(ctx);
 
-	/* release our connection and context */
 	xmpp_conn_release(conn);
 	xmpp_ctx_free(ctx);
 
-	/* final shutdown of the library */
 	xmpp_shutdown();
 
 	return 0;
